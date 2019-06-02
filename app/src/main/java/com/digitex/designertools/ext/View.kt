b074bb7b775aa2b1403b8ba9be3e1ebe5ffe8e40
Artur Sechko <artur.sechko@effective-soft.com>
2019-06-03 01:51:38 +0300

    Render content to full screen

--- app/src/main/java/com/digitex/designertools/ext/View.kt
+++ app/src/main/java/com/digitex/designertools/ext/View.kt
@@ -1,27 +1,11 @@
 package com.digitex.designertools.ext
 
-import android.graphics.Bitmap
 import android.view.View
-import android.widget.FrameLayout
-import androidx.core.view.drawToBitmap
+import android.view.WindowInsets
 import androidx.dynamicanimation.animation.DynamicAnimation
 import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
 import androidx.dynamicanimation.animation.withSpringForceProperties
-
-fun View.renderToBitmap(): Bitmap {
-    layoutParams = FrameLayout.LayoutParams(
-            FrameLayout.LayoutParams.WRAP_CONTENT,
-            FrameLayout.LayoutParams.WRAP_CONTENT
-    )
-    measure(
-            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
-            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-    )
-    layout(0, 0, measuredWidth, measuredHeight)
-
-    return drawToBitmap()
-}
 
 fun View.spring(
         property: DynamicAnimation.ViewProperty,
@@ -39,5 +23,54 @@ fun View.spring(
         this.stiffness = stiffness
         this.dampingRatio = dampingRatio
         this.finalPosition = finalPosition
+    }
+}
+
+fun View.doOnAttachToWindow(action: (view: View) -> Unit) {
+    addOnAttachStateChangeListener(onAttach = action)
+}
+
+fun View.doOnDettachFromWindow(action: (view: View) -> Unit) {
+    addOnAttachStateChangeListener(onDetach = action)
+}
+
+private inline fun View.addOnAttachStateChangeListener(
+        crossinline onAttach: (view: View) -> Unit = {},
+        crossinline onDetach: (view: View) -> Unit = {}
+) = addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+    override fun onViewAttachedToWindow(view: View) {
+        view.removeOnAttachStateChangeListener(this)
+        onAttach(view)
+    }
+
+    override fun onViewDetachedFromWindow(view: View) {
+        view.removeOnAttachStateChangeListener(this)
+        onDetach(view)
+    }
+})
+
+fun View.doOnApplyWindowInsets(action: (View, WindowInsets, InitialPadding) -> Unit) {
+    val initialPadding = InitialPadding(paddingLeft, paddingTop, paddingRight, paddingBottom)
+    // Set an actual OnApplyWindowInsetsListener which proxies to the given
+    // lambda, also passing in the original padding state
+    setOnApplyWindowInsetsListener { view, insets ->
+        action(view, insets, initialPadding)
+        // Always return the insets, so that children can also use them
+        insets
+    }
+    // request some insets
+    requestApplyInsetsWhenAttached()
+}
+
+data class InitialPadding(val left: Int, val top: Int,
+                          val right: Int, val bottom: Int)
+
+private fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        requestApplyInsets()
+    } else {
+        doOnAttachToWindow {
+            requestApplyInsets()
+        }
     }
 }
--- app/src/main/java/com/digitex/designertools/ui/DesignerToolsActivity.kt
+++ app/src/main/java/com/digitex/designertools/ui/DesignerToolsActivity.kt
@@ -1,8 +1,11 @@
 package com.digitex.designertools.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.updatePadding
 import com.digitex.designertools.R
+import com.digitex.designertools.ext.doOnApplyWindowInsets
 import com.digitex.designertools.utils.isCyanogenMod
 import kotlinx.android.synthetic.main.activity_designer_tools.*
 
@@ -13,6 +16,16 @@ class DesignerToolsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_designer_tools)
         if (!isCyanogenMod()) {
             qsTilesTextView.setText(R.string.overlays_section_text)
+        }
+
+        rootView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
+        contentView.doOnApplyWindowInsets { view, insets, padding ->
+            view.updatePadding(
+                    bottom = padding.bottom + insets.systemWindowInsetBottom,
+                    top = padding.top + insets.systemWindowInsetTop
+            )
         }
     }
 }
